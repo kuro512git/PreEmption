cea251a7f2437201082b640acd9778ad10e7f84b
Yuki Ishiguro <kuro512@gmail.com>
2021-03-09 21:50:04 +0900

    少々修正

---
+++ Assets/Scripts/Opengate.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opengate : MonoBehaviour
+{
+    //HingeJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = 50;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //左を動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftGate")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //右を動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightGate")
+        {
+            SetAngle(this.flickAngle);
+        }
+        /*
+        //矢印キー離された時フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        */
+    }
+
+    //とびら傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+}
